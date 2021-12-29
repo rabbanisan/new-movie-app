13b74657bd790093dbbce627425622ead38fe1dc
M Iqbal Kurniawan <iqbalkurniawan934@gmail.com>
2021-12-29 11:30:12 +0700

    Movie app initial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'assets/scss/bootstrap/bootstrap-grid.scss';
 import 'assets/scss/default.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from 'pages/Home';
+import Home from 'pages/Movie/Home';
 import Datepickers from 'pages/Datepickers';
 import InfiniteScroll from 'pages/InfiniteScroll';
 import ReactTabs from 'pages/ReactTabs';
---
+++ src/pages/Movie/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'pages/Movie/Home/style.scss';
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
